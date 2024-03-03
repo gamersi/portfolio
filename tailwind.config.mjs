e106bb3d7211be071633c972b3133c8491098ec5
Simon <hello@gamersi.at>
2024-03-03 16:23:47 +0100

    update default fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', 'Inter', 'sans-serif'],
+                serif: ['Merriweather', 'serif'],
+            },
+        },
     },
     plugins: [require('@tailwindcss/typography')],
     darkMode: 'class',
