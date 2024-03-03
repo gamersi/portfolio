76e6794133a982b52ffd1290abb0065dfb981f82
Simon <hello@gamersi.at>
2024-03-03 16:11:44 +0100

    add tailwind, update styles

--- src/pages/posts/welcome-here.md
+++ src/pages/posts/welcome-here.md
@@ -1,16 +1,17 @@
 ---
 layout: ../../layouts/MdPostLayout.astro
-title: "Welcome to my portfolio"
+title: 'Welcome to my portfolio'
 pubDate: 02.03.2024
-description: "This is my portfolio, where I will be sharing my projects and thoughts."
-author: "gamersi"
+description: 'This is my portfolio, where I will be sharing my projects and thoughts.'
+author: 'gamersi'
 image:
-  hasImage: false
-lang: "English"
-tags: ["portfolio", "welcome"]
+    hasImage: false
+lang: 'English'
+tags: ['portfolio', 'welcome']
 ---
 
 # Welcome to my blog!
+
 I am a software developer and student at HTL Villach in Austria. I am passionate about software development and I am always looking for new projects to work on.
 
 I will be sharing my projects and thoughts on this blog. I hope you enjoy your stay!
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,11 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context: any) {
-  return rss({
-    title: "Simon (aka gamersi) Portfolio and Blog",
-    description: "A blog and portfolio site for Simon (aka gamersi)",
-    site: context.site,
-    customData: `<language>en-US</language>`, // @ts-ignore
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
-  });
+    return rss({
+        title: 'Simon (aka gamersi) Portfolio and Blog',
+        description: 'A blog and portfolio site for Simon (aka gamersi)',
+        site: context.site,
+        customData: `<language>en-US</language>`, // @ts-ignore
+        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    });
 }
