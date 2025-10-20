26913e5f05feb9c959532354cc0eb5199081f393
Simon <hello@gamersi.at>
2025-10-20 21:27:18 +0200

    fix rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context: any) {
+    const posts = await getCollection('blog');
     return rss({
         title: 'Simon (aka gamersi) Portfolio and Blog',
         description: 'A blog and portfolio site for Simon (aka gamersi)',
         site: context.site,
-        customData: `<language>en-US</language>`, // @ts-ignore
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items: posts.sort((a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime()).map((post) => ({
+            title: post.data.title,
+            link: `/posts/${post.id}`,
+            pubDate: post.data.pubDate,
+            author: post.data.author,
+            categories: post.data.tags,
+        })),
     });
 }
