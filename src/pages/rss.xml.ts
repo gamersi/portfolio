b9d7e59363f614b1c0cab21f47b0e4e45108481a
Simon <hello@gamersi.at>
2025-10-20 21:33:28 +0200

    Enhance RSS feed by adding custom data link and updating author information

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -7,11 +7,12 @@ export async function GET(context: any) {
         title: 'Simon (aka gamersi) Portfolio and Blog',
         description: 'A blog and portfolio site for Simon (aka gamersi)',
         site: context.site,
+        customData: `<atom:link href="${context.site}/rss.xml" rel="self" type="application/rss+xml" />`,
         items: posts.sort((a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime()).map((post) => ({
             title: post.data.title,
             link: `/posts/${post.id}`,
             pubDate: post.data.pubDate,
-            author: post.data.author,
+            author: 'gamersi <simon@gamersi.at>',
             categories: post.data.tags,
         })),
     });
